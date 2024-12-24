f7c87c452e0a11d951abfebc5f7f9a07c019b170
pratikshatiwari <47111738+pratikshatiwari@users.noreply.github.com>
2024-12-24 09:12:12 +0530

    Create sqlinjection1.java

---
+++ src/main/java/sqlinjection1.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class VulnerableJava {
+    public void getUser(String username) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "password");
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM users WHERE username = '" + username + "'";
+            ResultSet resultSet = statement.executeQuery(query); // Vulnerable to SQL Injection
+            while (resultSet.next()) {
+                System.out.println("User: " + resultSet.getString("username"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
