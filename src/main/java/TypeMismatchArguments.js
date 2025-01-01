439ba44fd6d04e3264f9b8888867ab0de6c37488
pratikshatiwari <47111738+pratikshatiwari@users.noreply.github.com>
2025-01-01 15:47:48 +0530

    Create TypeMismatchArguments.js

---
+++ src/main/java/TypeMismatchArguments.js
@@ -0,0 +1,39 @@
+// Function expecting specific argument types
+function calculateRectangleArea(length, width) {
+    if (typeof length !== 'number' || typeof width !== 'number') {
+        throw new Error("Both arguments must be numbers.");
+    }
+    return length * width;
+}
+
+// Function expecting a callback function as an argument
+function executeCallback(callback) {
+    if (typeof callback !== 'function') {
+        throw new Error("Argument must be a function.");
+    }
+    callback();
+}
+
+// Main code
+try {
+    // Correct function call
+    console.log("Area:", calculateRectangleArea(10, 5));
+
+    // Incorrect: Passing a string instead of a number
+    console.log("Area:", calculateRectangleArea("10", 5));
+
+    // Incorrect: Passing an object instead of a number
+    console.log("Area:", calculateRectangleArea({ length: 10 }, 5));
+
+    // Correct function call
+    executeCallback(() => console.log("Callback executed!"));
+
+    // Incorrect: Passing a string instead of a function
+    executeCallback("This is not a function");
+
+    // Incorrect: Passing an array instead of a function
+    executeCallback(["This", "is", "not", "a", "function"]);
+
+} catch (error) {
+    console.error("Error:", error.message);
+}
