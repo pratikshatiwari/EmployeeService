61fd832ac0aca6bcf12193042cb1e2711bd0fa9a
pratikshatiwari <47111738+pratikshatiwari@users.noreply.github.com>
2025-01-02 21:17:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Used dependency: lodash
+const _ = require('lodash');
+
+// Unnecessary: moment and uuid are not used in the project
+// const moment = require('moment');
+// const { v4: uuidv4 } = require('uuid');
+
+// Example usage of lodash (used dependency)
+const array = [1, 2, 3, 4, 5];
+const shuffledArray = _.shuffle(array);
+console.log('Shuffled Array:', shuffledArray);
+
+// Code doesn't need the moment or uuid libraries
